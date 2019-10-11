a7abdf77aa8dde8f9d7b38b2129b5234efef01f6
cleilton <c.cleiltonoliveira@bol.com.br>
2019-10-11 19:38:29 -0300

    Paging in REST requests

--- src/main/java/br/com/springboot/repository/StudentRepository.java
+++ src/main/java/br/com/springboot/repository/StudentRepository.java
@@ -6,12 +6,14 @@ package br.com.springboot.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.springboot.model.Student;
 
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
+//
+//public interface StudentRepository extends CrudRepository<Student, Long> {
+//	public List<Student> findByNameIgnoreCaseContaining(String name);
+//}
+public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
 	public List<Student> findByNameIgnoreCaseContaining(String name);
-
 }
